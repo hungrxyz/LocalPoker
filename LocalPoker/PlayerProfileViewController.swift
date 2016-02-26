04662da43d32e02d576f664925bae84d23f3ac97
zzzel <marko@zzzel.xyz>
2016-02-26 20:15:29 +0100

    COnfigured Player to use pokerName as the recordID for Player record

--- LocalPoker/PlayerProfileViewController.swift
+++ LocalPoker/PlayerProfileViewController.swift
@@ -14,7 +14,7 @@ class PlayerProfileViewController: UIViewController {
 	@IBOutlet weak var nameTextField: UITextField!
 	@IBOutlet weak var pokerNameTextField: UITextField!
 	
-	var userRecordID: String?
+	var pokerName: String?
 	var userRecord: CKRecord? {
 		didSet {
 			dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -28,17 +28,31 @@ class PlayerProfileViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		if !playerProfileExists() {
+		pokerName = NSUserDefaults.standardUserDefaults().valueForKey("pokerName") as? String
+		
+		if pokerName == nil {
 			CKContainer.defaultContainer().fetchUserRecordIDWithCompletionHandler { userRecord, error in
 				if let error = error {
 					print(error)
 				} else if let userRecord = userRecord {
-					self.userRecordID = String(userRecord.recordName.characters.dropFirst())
 					print(userRecord.recordName)
+					let query = CKQuery(recordType: "Player", predicate: NSPredicate(format: "userRecordID == %@", userRecord.recordName))
+					self.publicDatabase.performQuery(query, inZoneWithID: nil, completionHandler: { (records, error) -> Void in
+						if let error = error {
+							print(error)
+							print("Player not registered yet")
+						} else if let records = records {
+							if let record = records.first {
+								self.userRecord = record
+								self.pokerName = record.recordID.recordName
+							}
+						}
+					})
 				}
 			}
-		} else {
-			let recordID = CKRecordID(recordName: NSUserDefaults.standardUserDefaults().valueForKey("userRecordID") as! String)
+		} else if let pokerName = pokerName {
+			pokerNameTextField.enabled = false
+			let recordID = CKRecordID(recordName: pokerName)
 			publicDatabase.fetchRecordWithID(recordID) { record, error in
 				if let error = error {
 					print(error)
@@ -51,13 +65,14 @@ class PlayerProfileViewController: UIViewController {
 	}
 	
 	@IBAction func saveTapped(sender: AnyObject) {
-		if !pokerNameTextField.text!.isEmpty {
-			if playerProfileExists() {
-				saveUserRecord(userRecord!)
-			} else {
-				let record = CKRecord(recordType: "Player", recordID: CKRecordID(recordName: userRecordID!))
-				saveUserRecord(record)
-			}
+		if let userRecord = userRecord {
+			userRecord.setValue(nameTextField.text, forKey: "name")
+			saveUserRecord(userRecord)
+		} else if let text = pokerNameTextField.text {
+			let record = CKRecord(recordType: "Player", recordID: CKRecordID(recordName: text))
+			record.setValue(nameTextField.text, forKey: "name")
+			record.setValue(pokerNameTextField.text, forKey: "pokerName")
+			saveUserRecord(record)
 		} else {
 			let alertController = UIAlertController(title: "Incomplete Profile", message: "Poker Name is required", preferredStyle: .Alert)
 			alertController.addAction(UIAlertAction(title: "Okay", style: .Default, handler: nil))
@@ -66,36 +81,15 @@ class PlayerProfileViewController: UIViewController {
 	}
 	
 	func saveUserRecord(record: CKRecord) {
-		record.setValue(nameTextField.text, forKey: "name")
-		record.setValue(pokerNameTextField.text, forKey: "pokerName")
 		publicDatabase.saveRecord(record) { record, error in
 			if let error = error {
 				print(error)
 			} else if let record = record {
-				if !self.playerProfileExists() {
-					NSUserDefaults.standardUserDefaults().setValue(record.recordID.recordName, forKey: "userRecordID")
+				if self.pokerName == nil {
+					NSUserDefaults.standardUserDefaults().setValue(record.recordID.recordName, forKey: "pokerName")
 				}
 				print(record)
 			}
 		}
 	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
-	func playerProfileExists() -> Bool {
-		return NSUserDefaults.standardUserDefaults().valueForKey("userRecordID") == nil ? false : true
-	}
-	/*
-	// MARK: - Navigation
-	
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-	// Get the new view controller using segue.destinationViewController.
-	// Pass the selected object to the new view controller.
-	}
-	*/
-	
 }
